c76b63baa93f47ec182e827731480a9b5bdf747a
lorena_olay <olaylorena2002@gmail.com>
2021-11-17 14:45:49 +0100

    condiciones mal

--- src/main/java/com/mycompany/generadorcontrasenas/CONTRASEÑAS.java
+++ src/main/java/com/mycompany/generadorcontrasenas/CONTRASEÑAS.java
@@ -4,17 +4,58 @@
  */
 package com.mycompany.generadorcontrasenas;
 
+import static java.lang.Math.random;
+import java.util.Random;
+
 /**
  *
  * @author lorena
  */
 public class CONTRASEÑAS {
 
+    public static boolean numeroImpar(int n1, int n4) {
+        int suma = n1 + n4;
+        int resto = suma % 2;
+        boolean impar = resto == 0;
+        return impar;
+    }
+
+    public static boolean numeroPar(int n2, int n3) {
+        int multiplicacion = n2 * n3;
+        int resultado = multiplicacion % 2;
+        boolean par = resultado != 0;
+        return par;
+    }
+
+    public static char letraAleatoria() {
+        Random random = new Random();
+        String letra = "FGHIJKLMNÑOPQRSTUVWX";
+        int randomInt = random.nextInt(letra.length());
+        char randomChar = letra.charAt(randomInt);
+        return randomChar;
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int n1, n2, n3, n4;
+        Random numAleatorio = new Random();
+        char letra = letraAleatoria();
         // TODO code application logic here
+        do {
+            n1 = numAleatorio.nextInt(10);
+            n2 = numAleatorio.nextInt(10);
+            n3 = numAleatorio.nextInt(10);
+            n4 = numAleatorio.nextInt(10);
+            System.out.println(n1 + " " + n2 + " " + n3 + " " + n4 + " " + letra);
+
+        } while (numeroPar(n2, n3) || numeroImpar(n1, n4) 
+                || n1 != n2 || n1 != n3 || n1 != n4
+                || n2 != n1 ||  n2 != n3 ||  n2 != n4
+                || n3 != n1 || n3 != n2 || n3 != n4
+                || n4 != n1 || n4 != n2 || n4 != n3);
+        System.out.println("LA ÚLTIMA CONTRASEÑA ES VÁLIDA");
     }
-    
+
 }
